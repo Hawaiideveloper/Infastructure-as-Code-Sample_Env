767d5440bb494ea0b637bac6fa41a4568f0a1dd5
Corey Albright <Hawaiideveloper@users.noreply.github.com>
2020-12-21 14:48:14 -0500

    Update AWS_CLI_installer.sh

--- AWS_CLI_installer.sh
+++ AWS_CLI_installer.sh
@@ -24,5 +24,5 @@ echo "it has been sourced and is ready for use"
 aws --version
 
 
+curl -LJO https://raw.githubusercontent.com/Hawaiideveloper/Sample_Env/blob/main/aws_creds_configure_step2.sh && chmod +x aws_creds_configure_step2.sh && ./aws_creds_configure_step2.sh
 
-https://github.com/Hawaiideveloper/Sample_Env/blob/main/aws_creds_configure_step2.sh
