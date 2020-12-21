718e277aac3453b5602ed70e1faac59b9bf759ef
Corey Albright <Hawaiideveloper@users.noreply.github.com>
2020-12-21 14:29:21 -0500

    Create Launching_EC2_Instances.sh

---
+++ Launching_EC2_Instances.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# Now we test our AWSCLI
+
+
+
+echo "Hopefully your AWS keys are setup correctly"
+
+echo "Now testing our AWS CLI"
+
+echo "Step 1 check versions"
+
+echo " Step 2 now we want to launch an instance"
+
+echo "Step 3 now we want to terminate that instance"
