2eea0bb33596940cd5d6c80a4c52fb237f710fce
Corey Albright <coreymalbright@gmail.com>
2021-04-19 11:49:28 -0400

    Adding getting started launch script

---
+++ Kubernetes/launch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+
+
+
+# Initialize the kubeadm 
+sudo kubeadm init
+
+
+# Provide cluster version
+kubectl version -o json
+
+
+# Provide cluster information
+kubectl 
+
+# Show current ip address
+ip add sh
+
+
+
+
