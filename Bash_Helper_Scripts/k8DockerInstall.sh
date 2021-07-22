cade7ece76d646638085a034c3805a7b68788a77
Corey Albright <coreyalbright@Coreys-MacBook-Pro.local>
2021-07-22 15:58:07 -0400

    docker installer

---
+++ Bash_Helper_Scripts/k8DockerInstall.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+
+
+
+
+
+
+
+
+
+
+# DOcker install
+
+sudo apt update -y
+sudo apt upgrade -y
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo docker run hello-world
+
+
+
+
+# Kubernetes Install
+sudo apt install -y curl wget apt-transport-https
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo cp minikube-linux-amd64 /usr/local/bin/minikube
+sudo chmod +x /usr/local/bin/minikube
+minikube version
+
+
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+
+chmod +x kubectl
+
+sudo mv kubectl /usr/local/bin/
+
+kubectl version -o yaml
+
+minikube start --driver=docker
+
+minikube start --addons=ingress --cpus=2 --cni=flannel --install-addons=true --kubernetes-version=stable --memory=2700
+
+# minikube status
+
+# kubectl cluster-info
+
+# kubectl get nodes
+
+
+minikube addons list
+
+minikube dashboard
+
+
