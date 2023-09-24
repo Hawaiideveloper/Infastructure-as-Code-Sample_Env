6bd6a1332b2272b73349a4980fd4c2e9b0feb00d
Corey Albright <hawaiideveloper@gmail.com>
2023-09-24 09:33:37 -0600

    gold for cleaning up divergent branches

--- gitlabServer/solve_merger_conflict.sh
+++ gitlabServer/solve_merger_conflict.sh
@@ -1,3 +1,39 @@
 git pull --tags origin main --no-rebase
 
 git pull --tags origin main --rebase
+
+git config --global pull.rebase true
+
+
+
+
+#The error message you received is because your local branch and the remote branch have 
+#diverged, meaning there are commits in your local branch that are not in the remote branch, 
+#and/or there are commits in the remote branch that are not in your local branch.
+#Here's how you can reconcile them, based on the options hinted in the error message:
+#This will merge the changes from the remote main branch into your local branch. 
+#Any conflicts will need to be resolved manually. This method will create a new merge commit if
+# there are changes in the local branch.
+
+
+#This will reapply your local commits on top of the incoming commits from the remote main branch. 
+#Any conflicts during this process will need to be resolved manually. 
+#Rebasing creates a linear commit history, but be cautious as it rewrites commit history.
+
+#This will only update the local branch if it is a fast-forward, meaning your local branch is strictly 
+#behind the remote branch and there are no divergent changes. 
+#If the branches have diverged, this command will fail, and you will need to use merge or rebase.
+
+#This sets rebase as the default behavior for git pull for all your repositories (due to the --global flag). 
+#You can replace true with false to set merging as the default behavior, 
+#or with only to set fast-forward only as the default behavior.
+
+
+
+#If you are unsure of the state of your branches and want to manually handle the divergence, 
+#you might want to fetch the changes first, 
+#review them, and then decide how to reconcile.
+
+#git fetch origin main
+#git log main..origin/main   # see what is in the remote branch that you don't have
+#git log origin/main..main   # see what you have that the remote branch doesn't have
